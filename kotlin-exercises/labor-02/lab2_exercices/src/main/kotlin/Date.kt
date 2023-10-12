ac97a405a51d1c7c7592fca891f796e7ac8fba03
Miklo Jozsef <miklo.jozsef.peter@student.ms.sapientia.ro>
2023-10-12 23:56:56 +0300

    lab2

--- kotlin-exercises/labor-02/lab2_exercices/src/main/kotlin/Date.kt
+++ kotlin-exercises/labor-02/lab2_exercices/src/main/kotlin/Date.kt
@@ -1,11 +1,31 @@
 import java.util.Calendar
 
-//data class Date (var year:Int = Calendar.YEAR,
-//                 var month:Int = Calendar.MONTH,
-//                 var day: Int = Calendar.DAY_OF_MONTH): Comparable<Date>{
-//
-//                     companion object{
-//
-//                     }
-//
-//}
+data class Date (var year:Int = Calendar.YEAR,
+                 var month:Int = Calendar.MONTH,
+                 var day: Int = Calendar.DAY_OF_MONTH) : Comparable<Date>{//: Comparable<Date>
+
+
+//    init {
+//        var year: Int
+//        var month: Int
+//        var day: Int
+//    }
+
+    override fun compareTo(other: Date): Int {
+        if (this.year != other.year)
+            return this.year-other.year
+
+        if (this.month != other.month) //year are the same
+            return this.month-other.month
+
+        return this.day-other.day   //year and month are the same
+    }
+    override fun toString(): String {
+        return "Date(year=$year, month=$month, day=$day)"
+    }
+
+    companion object{
+        val customDateComparator : Comparator<Date> = compareBy({it.year},{it.month}, {it.day})
+    }
+
+}
--- kotlin-exercises/labor-02/lab2_exercices/src/main/kotlin/Main.kt
+++ kotlin-exercises/labor-02/lab2_exercices/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.util.Random
 
 
 //2. Extension function - letezo osztalyt kibovit valamivel anelkul, hogy ujat szarmaztatnank
@@ -6,6 +7,29 @@ fun String.nameMonogram():String = this.split(" ").map{ it[0] }.joinToString("-"
 fun List<String>.joinedList(separator:String) = this.joinToString(separator)
 
 fun List<String>.longestElement() = this.maxByOrNull { it.length }
+
+fun Date.isLeap():Boolean = this.year % 4 ==0
+
+fun Date.isValid() :Boolean {
+    if(this.year<0 || this.year>2023) return false
+    if(this.month<1 || this.month>12) return false
+    if(this.day<1 || this.day>31) return false
+
+    //February
+    if(this.month==2){
+        if(this.isLeap()){
+            return this.day<=29   //leap year
+        }
+        else{
+            return this.day<=28
+        }
+    }
+
+    if(this.month==2 || this.month==4 || this.month==6 || this.month==9 || this.month==11){
+        return this.day<=30
+    }
+    return true
+}
 
 fun main(){
 
@@ -38,17 +62,59 @@ fun main(){
 
 
 
-   //Problem 2.
-    //Monogram
-    val name:String="John Smith"
-    println(name.nameMonogram())
+//   //Problem 2.
+//    //Monogram
+//    val name:String="John Smith"
+//    println(name.nameMonogram())
+//
+//    //byseparator
+//    val list:List<String> = listOf("apple","pear","melon")
+//    val separator:String = "#"
+//    println(list.joinedList(separator))
+//
+//    //longest element
+//    val list2:List<String> = listOf("apple","pear","strawberry","melon")
+//    println(list2.longestElement())
 
-    //byseparator
-    val list:List<String> = listOf("apple","pear","melon")
-    val separator:String = "#"
-    println(list.joinedList(separator))
+    //Problem 3.
+    var i=0
+    val dates = mutableListOf<Date>()
+    while(i<10){
+        val random = Random()
+        val year = 1900 + random.nextInt(200)
+        val month = 3 + random.nextInt(12)
+        val day = 10 + random.nextInt(25)
+        val date:Date = Date(year,month,day)
+        if(date.isValid()){
+            ++i;
+            dates.add(date);
+        }
+        else{
+            println("Not valid date: ${date.toString()}")
+        }
+    }
+    dates.forEach {
+        println("Valid date: ${it.toString()}")
+    }
 
-    //longest element
-    val list2:List<String> = listOf("apple","pear","strawberry","melon")
-    println(list2.longestElement())
+    //sort by natural ordering
+    dates.sort()
+    println("Sorted list:")
+    dates.forEach {
+        println(it.toString())
+    }
+
+    dates.sortDescending()
+    println("Sorted reverse:")
+    dates.forEach {
+        println(it.toString())
+    }
+
+    //Sort by using a custom ordering
+    dates.sortWith(Date.customDateComparator);
+    println("Sorted with custom oreding:")
+    dates.forEach {
+        println(it.toString())
+    }
 }
+
