e9cdb2c5788a3075ffdbb057e8f0a0d45feb0c7b
Miklo Jozsef <miklo.jozsef.peter@student.ms.sapientia.ro>
2023-10-28 15:51:46 +0300

    Recipe Hub

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.tasty.recipesapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tasty.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        Log.d(TAG, "##onCreate: MainActivity created.")
 
-        //val binding = ActivityMainBinding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView((binding.root))    //vagy az elozo sor setContentView
 
-        val result = intent.getStringExtra("Message")
-        //Log.d(TAG,"MainAactivity - result = $result")
+
+//        val result = intent.getStringExtra("justAtext")
+//        binding.textViewMain.text= result
+//        Log.d(TAG,"##MainAactivity - result = $result")
     }
 }
--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -1,27 +1,75 @@
 package com.tasty.recipesapp
 
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
+import com.tasty.recipesapp.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        //setContentView((binding.root))    //vagy az elozo sor
-    }
+        Log.d("SplashActivity", "##onCreate called")
+        //setContentView(R.layout.activity_splash)
 
-//    binding.startButton.setOnClickListener{
-//        Log.d()
-//         val
-//    }
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView((binding.root))    //vagy az elozo sor setContentView
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("SplashActivity", "onDestroy called")
+//        binding.startButton.setOnClickListener{
+//            Log.d(TAG, "##startButton onClick")
+//            val intent = Intent(this, MainActivity::class.java)
+//            val text = binding.editTextText.getText().toString()
+//            intent.putExtra("justAtext",text)
+//            startActivity(intent)
+//        }
+
+        // Use a HandlerThread to create a background thread
+        val handlerThread = HandlerThread("SplashHandlerThread", -10)
+        handlerThread.start() // Create a Handler on the new HandlerThread
+        val handler = Handler(handlerThread.looper)
+        val SPLASH_TIME_OUT:Long=2000;
+        handler.postDelayed({
+        // Navigate to MainActivity after the delay
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish() },
+            SPLASH_TIME_OUT)
+
+
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("SplashActivity", "##onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SplashActivity", "##onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SplashActivity", "##onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SplashActivity", "##onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SplashActivity", "##onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("SplashActivity", "##onRestart called")
     }
 }
